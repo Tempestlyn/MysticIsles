903042499e72592f623dd6f717d57e31530b4269
Tempestlyn <31550496+Tempestlyn@users.noreply.github.com>
2022-11-24 22:16:03 -0500

    Any entity can be possessed by the player controller. AI additions.

--- Mystic Isles/Assets/SelectPieceButton.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SelectPieceButton : MonoBehaviour
-{
-    public int PieceID;
-
-}
---
+++ MythicIsles 5.1/Intermediate/Build/Win64/MythicIslesEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_0.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30141
+#include "C:/Users/ringe/Documents/GitHub/Mystic Isles/MythicIsles 5.1/Intermediate/Build/Win64/MythicIslesEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_0.h"
