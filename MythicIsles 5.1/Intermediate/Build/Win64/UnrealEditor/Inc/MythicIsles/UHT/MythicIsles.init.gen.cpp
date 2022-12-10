dd59f3037174dfa32f55cb7286d6c926e4b4d1dd
Tempestlyn <31550496+Tempestlyn@users.noreply.github.com>
2022-12-10 17:13:51 -0500

    Ability Structures made. Animation work.

---
+++ MythicIsles 5.1/Intermediate/Build/Win64/UnrealEditor/Inc/MythicIsles/UHT/MythicIsles.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeMythicIsles_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_MythicIsles;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_MythicIsles()
+	{
+		if (!Z_Registration_Info_UPackage__Script_MythicIsles.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/MythicIsles",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0xF5903A96,
+				0x86E6B4FF,
+				METADATA_PARAMS(nullptr, 0)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_MythicIsles.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_MythicIsles.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_MythicIsles(Z_Construct_UPackage__Script_MythicIsles, TEXT("/Script/MythicIsles"), Z_Registration_Info_UPackage__Script_MythicIsles, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xF5903A96, 0x86E6B4FF));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ MythicIsles 5.1/Source/MythicIsles/MovingPlatform.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+#include "Engine.h"
+// Sets default values
+AMovingPlatform::AMovingPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = Root;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	this->Mesh->AttachToComponent(this->RootComponent, FAttachmentTransformRules::KeepWorldTransform);
+
+
+	
+}
+
+// Called when the game starts or when spawned
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
